b6925c87626e27f952203772596026de2595c45b
Erik Vieczorek <erikvieczorek@gmail.com>
2023-11-23 09:52:19 -0300

    primeiro commit

---
+++ src/exercicio_ts.js
@@ -0,0 +1,14 @@
+"use strict";
+// Função de multiplicação
+function multiplicar(a, b) {
+    return a * b;
+}
+// Função de saudação
+function saudacao(nome) {
+    return "Olá " + nome + "!";
+}
+// Exemplos de uso
+const resultadoMultiplicacao = multiplicar(1.772455923288362, 1.772455923288362);
+const mensagemSaudacao = saudacao("Ebac");
+console.log(mensagemSaudacao);
+console.log("O resultado da multiplicação é:", resultadoMultiplicacao);
---
+++ src/exercicio_ts.ts
@@ -0,0 +1,18 @@
+    // Função de multiplicação
+function multiplicar(a: number, b: number): number {
+    return a * b;
+}
+
+
+    // Função de saudação
+function saudacao(nome: string): string {
+    return "Olá " + nome + "!";
+}
+
+
+    // Exemplos de uso
+const resultadoMultiplicacao = multiplicar(1.772455923288362, 1.772455923288362);
+const mensagemSaudacao = saudacao("Ebac");
+
+console.log(mensagemSaudacao);
+console.log("O resultado da multiplicação é:", resultadoMultiplicacao);
